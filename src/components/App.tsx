dbbf57981c7214ea111b54abf6a4c309c05466f0
amalker <it.amalker@yandex.ru>
2020-06-06 15:18:32 +0300

    add persons, filter states and add components to render

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
+import Table from './Table';
+import Filter from './Filter';
+import data from '../data/small_data_persons.json';
+
+type FilterType = {
+  filterBy: string;
+};
 
 const App: React.FC = () => {
-  return <div>Test render</div>;
+  const [persons, setPersons] = useState(data);
+  const [filter, setFilter] = useState<FilterType>({ filterBy: '' });
+  return (
+    <div>
+      <Filter filter={filter} setFilter={setFilter} />
+      <Table persons={persons} setPersons={setPersons} />
+    </div>
+  );
 };
 
 export default App;
